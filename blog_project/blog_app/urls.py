43f8ed6d0518eda866643c2a4939e0646d91cc40
jose-uchoa <j-uchoa@hotmail.com>
2023-05-20 22:47:23 -0300

    feat: create route of the post detail

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from blog_app import views
 
 urlpatterns = [
     path("", views.PostView.as_view(), name="home"),
+    path("/<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
 ]
